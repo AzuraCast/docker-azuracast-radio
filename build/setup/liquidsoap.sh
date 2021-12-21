20a3fbafc8f7f7d6a81cd58ddaec703d4633095e
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-12-21 08:18:09 -0600

    Update deps to cover both Deb and OPAM builds.

--- build/setup/liquidsoap.sh
+++ build/setup/liquidsoap.sh
@@ -3,8 +3,12 @@ set -e
 source /bd_build/buildconfig
 set -x
 
-# Only install Liquidsoap deps (Liquidsoap build is handled by another container).
-$minimal_apt_get_install libfaad-dev libfdk-aac-dev libflac-dev libmad0-dev libmp3lame-dev libogg-dev \
-    libopus-dev libpcre3-dev libtag1-dev libsamplerate0-dev libavcodec-dev libavdevice-dev libavfilter-dev \
-    libavformat-dev libavresample-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev \
-    frei0r-plugins-dev ladspa-sdk multimedia-audio-plugins swh-plugins tap-plugins lsp-plugins-ladspa
+# Packages required by Liquidsoap
+$minimal_apt_get_install libao-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev \
+    libavutil-dev libfaad-dev libfdk-aac-dev libflac-dev libfreetype-dev libgd-dev libjack-dev \
+    libjpeg-dev liblo-dev libmad0-dev libmagic-dev libmp3lame-dev libopus-dev libpng-dev libportaudio2 \
+    libpulse-dev libsamplerate0-dev libsdl2-dev libsdl2-ttf-dev libshine-dev libsoundtouch-dev libspeex-dev \
+    libsrt-dev libswresample-dev libswscale-dev libtag1-dev libtheora-dev libtiff-dev libx11-dev libxpm-dev bubblewrap
+
+# Optional audio plugins
+$minimal_apt_get_install frei0r-plugins-dev ladspa-sdk multimedia-audio-plugins swh-plugins tap-plugins lsp-plugins-ladspa
