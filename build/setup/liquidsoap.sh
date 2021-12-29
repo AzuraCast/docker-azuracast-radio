2d8c96b21b92f468e0b22c9194ede880d4aafed3
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-12-29 17:55:57 -0600

    Add libsdl2-image-dev dependency.

--- build/setup/liquidsoap.sh
+++ build/setup/liquidsoap.sh
@@ -7,7 +7,7 @@ set -x
 $minimal_apt_get_install libao-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev \
     libavutil-dev libfaad-dev libfdk-aac-dev libflac-dev libfreetype-dev libgd-dev libjack-dev \
     libjpeg-dev liblo-dev libmad0-dev libmagic-dev libmp3lame-dev libopus-dev libpng-dev libportaudio2 \
-    libpulse-dev libsamplerate0-dev libsdl2-dev libsdl2-ttf-dev libshine-dev libsoundtouch-dev libspeex-dev \
+    libpulse-dev libsamplerate0-dev libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libshine-dev libsoundtouch-dev libspeex-dev \
     libsrt-dev libswresample-dev libswscale-dev libtag1-dev libtheora-dev libtiff-dev libx11-dev libxpm-dev bubblewrap
 
 # Optional audio plugins
