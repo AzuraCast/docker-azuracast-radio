a6ef79db027482df869ecfc53b984b026cc6de6b
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-02-01 19:03:29 -0600

    Update LS to 2.0.3-preview and update OPAM deps.

--- liquidsoap_amd64/build_as_azuracast.sh
+++ liquidsoap_amd64/build_as_azuracast.sh
@@ -7,11 +7,11 @@ opam init --disable-sandboxing -a --bare && opam switch create 4.12.0
 # Pin specific commit of Liquidsoap
 git clone --recursive https://github.com/savonet/liquidsoap.git /tmp/liquidsoap
 cd /tmp/liquidsoap
-git checkout 7d209e52e62bd0f0a59195fb4921e400de5ae97b
+git checkout 59795a505728bc6066e6915d8f6b996d39014af6
 opam pin add --no-action liquidsoap .
 
-opam install -y ladspa.0.2.0 ffmpeg.1.1.1 ffmpeg-avutil.1.1.1 ffmpeg-avcodec.1.1.1 ffmpeg-avdevice.1.1.1 \
+opam install -y ladspa.0.2.2 ffmpeg.1.1.1 ffmpeg-avutil.1.1.1 ffmpeg-avcodec.1.1.1 ffmpeg-avdevice.1.1.1 \
     ffmpeg-av.1.1.1 ffmpeg-avfilter.1.1.1 ffmpeg-swresample.1.1.1 ffmpeg-swscale.1.1.1 frei0r.0.1.2 \
-    samplerate.0.1.6 taglib.0.3.7 mad.0.5.0 faad.0.5.0 fdkaac.0.3.2 lame.0.3.5 vorbis.0.8.0 cry.0.6.5 \
-    flac.0.3.0 opus.0.2.0 dtools.0.4.4 duppy.0.9.2 ocurl.0.9.1 ssl \
+    samplerate.0.1.6 taglib.0.3.8 mad.0.5.2 faad.0.5.0 fdkaac.0.3.2 lame.0.3.5 vorbis.0.8.0 cry.0.6.6 \
+    flac.0.3.0 opus.0.2.1 dtools.0.4.4 duppy.0.9.2 ocurl.0.9.2 ssl.0.5.10 \
     liquidsoap
