fb2d8736ad4c3b23436936669d2e5abb62bc20b5
Buster "Silver Eagle" Neece <buster@busterneece.com>
2022-02-09 00:17:45 -0600

    Compact the final LS build size in stations container.

--- liquidsoap_amd64/build_as_azuracast.sh
+++ liquidsoap_amd64/build_as_azuracast.sh
@@ -7,7 +7,7 @@ opam init --disable-sandboxing -a --bare && opam switch create 4.12.0
 # Pin specific commit of Liquidsoap
 git clone --recursive https://github.com/savonet/liquidsoap.git /tmp/liquidsoap
 cd /tmp/liquidsoap
-git checkout ec8474f1c91a745446c120a883f825bfb8892ec8
+git checkout 759a926b3febaaa48a852ae20f8ae29c83b40ba8
 opam pin add --no-action liquidsoap .
 
 opam install -y ladspa.0.2.2 ffmpeg.1.1.1 ffmpeg-avutil.1.1.1 ffmpeg-avcodec.1.1.1 ffmpeg-avdevice.1.1.1 \
@@ -15,3 +15,14 @@ opam install -y ladspa.0.2.2 ffmpeg.1.1.1 ffmpeg-avutil.1.1.1 ffmpeg-avcodec.1.1
     samplerate.0.1.6 taglib.0.3.8 mad.0.5.2 faad.0.5.0 fdkaac.0.3.2 lame.0.3.5 vorbis.0.8.0 cry.0.6.6 \
     flac.0.3.0 opus.0.2.1 dtools.0.4.4 duppy.0.9.2 ocurl.0.9.2 ssl.0.5.10 \
     liquidsoap
+
+# Have Liquidsoap build its own chroot.
+mkdir -p /tmp/liquidsoap
+
+/var/azuracast/.opam/4.12.0/bin/liquidsoap /ls_build/liquidsoap/build_chroot.liq || true
+
+# Clear entire OPAM directory
+rm -rf /var/azuracast/.opam
+
+cp -r /tmp/liquidsoap/var/azuracast/.opam /var/azuracast/.opam
+rm -rf /tmp/liquidsoap
